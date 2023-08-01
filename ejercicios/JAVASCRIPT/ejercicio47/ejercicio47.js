e1c9a5c22d7fcb1d8ddc7a341a192c544baa3cfb
Moguete <esteveptt@gmail.com>
2023-08-01 13:30:35 +0200

    añado hasta el 57

---
+++ ejercicios/JAVASCRIPT/ejercicio47/ejercicio47.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers[0]);
+
+let lastindex = numbers.length - 1;
+
+console.log(numbers[lastindex]);
+
+let length = numbers.length;
+
+console.log(length);
+
+console.log(numbers[19]); //da indefinido ya que no existe ningun elemento definido en esa posición de la array
---
+++ ejercicios/JAVASCRIPT/ejercicio48/ejercicio48.js
@@ -0,0 +1,20 @@
+let students = [
+    obj1: {
+        id: 1,
+        name: "Esteve",
+        surname: "Mogas",
+        age: "28",
+    },
+    obj2 {
+    id: 2,
+    name: "Esteve",
+    surname: "Mogas",
+    age: "28",
+    },
+    obj3 {
+    id: 3,
+    name: "Esteve",
+    surname: "Mogas",
+    age: "28",
+    }
+]
