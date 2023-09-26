70d523d0cbcb19cbfd08fb1ad624979cd02f8f17
Moguete <esteveptt@gmail.com>
2023-09-26 17:16:57 +0200

    subo hasta ejercicio 3

---
+++ ejercicios/REACT/my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import JSX2 from "./ejercicios/2 JSX/2JSX";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+      <JSX2 name={"Esteve"}/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ ejercicios/REACT/my-app/src/ejercicios/2 JSX/2JSX.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JSX2 = ({name}) => {
+  return (
+    <h1>"Hello, {name}"</h1>
+  )
+}
+
+export default JSX2
