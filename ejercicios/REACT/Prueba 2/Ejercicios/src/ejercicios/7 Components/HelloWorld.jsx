8a3852b1b4bbc224b9ae6799186632ca70045aaf
Moguete <esteveptt@gmail.com>
2023-10-16 01:03:39 +0200

    Ejercicio 12

--- ejercicios/REACT/Prueba 2/Ejercicios/src/ejercicios/7 Components/HelloWorld.jsx
+++ ejercicios/REACT/Prueba 2/Ejercicios/src/ejercicios/7 Components/HelloWorld.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Message from "../12 Components/Message";
 
 const HelloWorld = () => {
   return (
     <div>
       <h1>Hello, world!</h1>
-      <p>What a beautiful day!</p>
+      <Message />
     </div>
   );
 };
