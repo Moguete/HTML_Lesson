6f45c84af08f88a2a546c9b5f7fdcc0f82bcbd86
Moguete <esteveptt@gmail.com>
2023-10-16 01:53:12 +0200

    Ejercicio 25

--- ejercicios/REACT/Prueba 2/Ejercicios/src/ejercicios/15 Props/Welcome.jsx
+++ ejercicios/REACT/Prueba 2/Ejercicios/src/ejercicios/15 Props/Welcome.jsx
@@ -5,7 +5,7 @@ const Welcome = ({ name, age }) => {
   return (
     <div>
       <p>Welcome, {name}!</p>
-      {age > 18 && age < 65 && name === "Jhon" && <Age age={age} />}
+      {age > 18 ? <p>Your age is {age}</p> : <p>You are very young!</p>}
     </div>
   );
 };
