fca8e13900b150c5841e80244267bed57431a788
Moguete <esteveptt@gmail.com>
2023-10-24 20:19:56 +0200

    Ejercicio 47

--- ejercicios/REACT/Prueba 2/Ejercicios/src/ejercicios/46 Uncontrolled Forms/UncontrolledLogin.jsx
+++ ejercicios/REACT/Prueba 2/Ejercicios/src/ejercicios/46 Uncontrolled Forms/UncontrolledLogin.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
 const UncontrolledLogin = () => {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const username = event.target.elements.username.value;
+    const password = event.target.elements.password.value;
+    const data = { username, password };
+    console.log(data);
+  }
   return (
-    <form>
+    <form onSubmit={handleFormSubmit}>
+      <h1>My Uncontrolled Form</h1>
       <input type="text" name="username" />
       <input type="password" name="password" />
       <input type="checkbox" name="session" />
