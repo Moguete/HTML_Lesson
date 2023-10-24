223d83e18743a87f7172ccff49347387e6d648ea
Moguete <esteveptt@gmail.com>
2023-10-24 19:19:13 +0200

    Ejercicio 38 - Se sigue mostrando porque está dentro del button y tambien hace el trigger del handleButton

--- ejercicios/REACT/Prueba 2/Ejercicios/src/ejercicios/36 Handling Events/MouseClicker.jsx
+++ ejercicios/REACT/Prueba 2/Ejercicios/src/ejercicios/36 Handling Events/MouseClicker.jsx
@@ -4,10 +4,12 @@ const MouseClicker = () => {
   function handleButton(e) {
     console.log(e.currentTarget.name);
   }
-
+  function handleImage(e) {
+    console.log(e.currentTarget.src);
+  }
   return (
     <button name="one" onClick={handleButton}>
-        <img width={50} height={50} src="/src/assets/Captura de pantalla 2023-09-06 a las 15.54.00.png" />
+        <img onClick={handleImage} width={50} height={50} src="/src/assets/Captura de pantalla 2023-09-06 a las 15.54.00.png" />
       click me!
     </button>
   );
