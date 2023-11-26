9187a2f3067a8879d11303a6b5e5ddb502032a4e
Moguete <esteveptt@gmail.com>
2023-11-26 21:37:21 +0100

    ejercicio 12 fixed

---
+++ ejercicios/NODE/12 CRUD/dist/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+const app = express();
+const port = 3000;
+let planets = [
+    {
+        id: 1,
+        name: "Earth",
+    },
+    {
+        id: 2,
+        name: "Mars",
+    },
+];
+app.use(morgan("dev"));
+app.use(express.json());
+app.get("/api/planets", (req, res) => {
+    res.status(200).json({ planets });
+});
+app.get("/api/planets/:id", (req, res) => {
+    const { id } = req.params;
+    const planet = planets.find(p => p.id === Number(id));
+    res.status(200).json(planet);
+});
+app.listen(port, () => {
+    console.log(`App listening on port http://localhost:${port}`);
+});
