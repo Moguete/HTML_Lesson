fec43caa3d5e089ffd1e78ae5385f24ae5212db9
Moguete <esteveptt@gmail.com>
2023-11-27 13:58:57 +0100

    repo creado checkpoint 4

---
+++ checkpoint 4/src/server.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+
+const app = express();
+const port = 3000;
+
+let baseDeDatos = [
+  {
+    id: 1,
+    name: "Marcoski",
+    city: "Madrid",
+  },
+  {
+    id: 2,
+    name: "Esteve",
+    city: "Barcelona",
+  },
+  {
+    id: 3,
+    name: "Erick",
+    city: "Tarragona",
+  },
+  {
+    id: 4,
+    name: "Marcos",
+    city: "Gijón",
+  },
+  {
+    id: 5,
+    name: "Barbara",
+    city: "Sevilla",
+  },
+];
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`App listening on port http://localhost:${port}`);
+});
